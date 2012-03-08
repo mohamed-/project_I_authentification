2688f669031c1ea256d089e1db8ed5d5bbcde325
Pierre Gambarotto <pierre.gambarotto@n7.fr>
2012-03-08 10:47:27 +0100

    sinatra spec for the registration scenario

--- app.rb
+++ app.rb
@@ -1,1 +1,22 @@
 # fichier principal de l'application Sinatra
+require 'sinatra'
+
+class User
+end
+
+get '/users/new' do 
+  erb :registration
+end
+
+post '/users' do 
+  u = User.create(params['user'])  
+  if u
+    redirect "/users/#{params['user']['login']}"
+  else
+    erb :registration
+  end
+end
+
+get "/users/:login" do 
+  "bonjoun #{login}"
+end
--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -1,5 +1,41 @@
+#encoding: UTF-8
+require_relative 'spec_helper'
 require_relative '../app.rb'
 
 describe "Authenticatin Service" do 
-  
+  describe "User registration" do 
+    describe "get /users/new" do 
+      it "should return a form to post registration info to /users" do 
+        get '/users/new'
+        last_response.should be_ok
+        last_response.body.should match %r{<form.*action="/users".*method="post".*}
+      end
+    end
+
+    describe "post /users" do
+      let(:params){ { "user" => {"login" => "toto", "name" => "Théodore Oto" }} }
+      it "should create a new user" do 
+        User.stub(:create)
+        User.should_receive(:create).with(params['user'])
+        post '/users', params
+      end
+
+      it "should redirect to user private page" do 
+        User.stub(:create){true}
+        post '/users', params
+        last_response.should be_redirect
+        follow_redirect!
+        last_request.path.should == '/users/toto'
+      end
+
+      context "creation went berserk" do 
+        it "should rerender the registration form" do 
+          User.stub(:create).and_return(false)
+          post '/users', params
+          last_response.should be_ok
+          last_response.body.should match %r{<form.*action="/users".*method="post".*}
+        end
+      end
+    end
+  end
 end
